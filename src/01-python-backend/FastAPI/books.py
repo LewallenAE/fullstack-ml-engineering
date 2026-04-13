854d9f14add1653a50f9a488d7755dbe7b1b8229
Anthony Lewallen <lewallenae@gmail.com>
2026-04-12 22:33:09 -0700

    Finished FastAPI project 1

--- src/01-python-backend/FastAPI/books.py
+++ src/01-python-backend/FastAPI/books.py
@@ -8,7 +8,11 @@ BOOKS = [
     {'title': 'Title three', 'author': 'Author three', 'category': 'english'},
     {'title': 'Title four', 'author': 'Author four', 'category': 'math'},
     {'title': 'Title five', 'author': 'Author five', 'category': 'computer science'},
-    {'title': 'Title six', 'author': 'Author six', 'category': 'machine learning'}
+    {'title': 'Title six', 'author': 'Author six', 'category': 'machine learning'},
+    {'title': 'Title seven', 'author': 'Author two', 'category': 'quantum physics'},
+    {'title': 'Title eight', 'author': 'Author two', 'category': 'renaissance art'},
+    {'title': 'Title nine', 'author': 'Author two', 'category': 'manga drawing'},
+    {'title': 'Title ten', 'author': 'Author two', 'category': 'teaching'},
 ]
 
 # all
@@ -17,7 +21,7 @@ async def read_all_books():
     return BOOKS
 
 # path parameter
-@app.get("/books/{category}")
+@app.get("/books/category/{category}")
 async def read_category(category: str):
     books_to_return = []
     for book in BOOKS:
@@ -38,7 +42,7 @@ async def read_category_query(category: str = None):
 
 
 @app.get("/book/search/")
-async def read_author_query(author: str =None):
+async def read_author_query(author: str = None):
     if author:
         books_to_return = []
         for book in BOOKS:
@@ -70,3 +74,25 @@ async def read_author_or_category_query(author:str = None, category: str = None)
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str) -> None:
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+@app.get("/books/author/{author}")
+async def get_book_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author', "").casefold().strip() == author.casefold().strip():
+            books_to_return.append(book)
+    return books_to_return
+
